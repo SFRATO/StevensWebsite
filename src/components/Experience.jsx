e4ad543b898460fba4be54024cb2ffc47bfe4d35
Steve Frato <35787831+SFRATO@users.noreply.github.com>
2025-06-23 17:07:29 -0400

    Update 39n.png logo in Experience component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -58,7 +58,7 @@ const Experience = () => {
               transition={{ type: "spring", stiffness: 300, damping: 20 }}
             >
               <div className="company-logo">
-                <img src="images/39n.png" alt="39 North Labs" />
+                <img src="/images/39n.png" alt="39 North Labs" />
               </div>
               <div className="experience-details">
                 <h3>Data Analysis Intern</h3>
@@ -82,7 +82,7 @@ const Experience = () => {
               transition={{ type: "spring", stiffness: 300, damping: 20 }}
             >
               <div className="school-logo">
-                <img src="images/stockton.png" alt="Stockton University" />
+                <img src="/images/stockton.png" alt="Stockton University" />
               </div>
               <div className="education-details">
                 <h3>Bachelor of Science in Biochemistry and Molecular Biology</h3>
